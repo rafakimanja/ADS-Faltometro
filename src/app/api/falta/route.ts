3b7661050b37b2a533a64e3179a3135cde19ab4f
Rafael Lopes Ens <rafaellopes.ifrs@gmail.com>
2025-06-30 11:06:11 -0300

    alterando rotas Falta

--- src/app/api/falta/route.ts
+++ src/app/api/falta/route.ts
@@ -1,5 +1,46 @@
-import { createFalta } from "@/services/falta_service";
+import { getDisciplinaById } from "@/services/disciplina_service";
+import { createFalta, getFaltas } from "@/services/falta_service";
 import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    const url = new URL(req.url)
+    const id_disc = url.searchParams.get('disciplina')
+    const idNum = Number(id_disc)
+
+    if (!id_disc) {
+        return new NextResponse("Parâmetro 'disciplina' é obrigatório", {
+        status: 400,
+        });
+    }
+
+    if(isNaN(idNum)) {
+        return new NextResponse("Invalid ID", {
+            status: 400,
+        })
+    }
+
+    const disc = getDisciplinaById(idNum)
+
+    if(!disc){
+        return new NextResponse("Disciplina não encontrada", {
+            status: 404, 
+        })
+    }
+
+    const faltas = getFaltas()
+
+    const faltasDaDisc = faltas.filter((falta) => falta.disciplinaID === disc.id)
+
+    if(faltasDaDisc.length > 0){
+        return new NextResponse(JSON.stringify(faltasDaDisc), {
+            status: 200,
+        })
+    } else {
+        return new NextResponse('Nenhuma falta registra nesta disciplina', {
+            status: 404,
+        })
+    }
+}
 
 export async function POST(req: NextRequest) {
     const body = await req.json()
